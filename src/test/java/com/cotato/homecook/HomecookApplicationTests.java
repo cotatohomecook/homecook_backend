b10ea76d2c1b0a919cd1ef9dda7fee2aa623e0ad
qjvk2880 <qjvk2880@gmail.com>
2023-04-29 15:34:15 +0900

    🎉 프로젝트 생성

---
+++ src/test/java/com/cotato/homecook/HomecookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotato.homecook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
