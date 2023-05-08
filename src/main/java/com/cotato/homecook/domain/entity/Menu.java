4a8ca44eb5f3ff958b719d9a802816b7032fc2a0
김민욱 <qjvk2880@naver.com>
2023-05-08 11:09:20 +0900

    🚑️ Menu entity 파일 재생성

---
+++ src/main/java/com/cotato/homecook/domain/entity/Menu.java
@@ -0,0 +1,40 @@
+package com.cotato.homecook.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long menuId;
+
+    // 가게랑 many to one , menu가 외래키 저장
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id", nullable = false)
+    private Shop shop;
+
+    // 태그랑 one to many
+    @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL)
+    List<Tag> tags = new ArrayList<>();
+
+    // 재료량 one to many
+    @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL)
+    List<Ingrediant> ingrediants  = new ArrayList<>();
+
+    private String menuName;
+    private String imageUrl;
+    private Long price;
+    private String description;
+
+}
