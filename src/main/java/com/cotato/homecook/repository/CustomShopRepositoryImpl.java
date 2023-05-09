0c4c1451dcdd6dfcac8afd4fee5c41c9e035119d
김민욱 <qjvk2880@naver.com>
2023-05-09 21:12:55 +0900

    🚧️ push를 위한 주석 처리

--- src/main/java/com/cotato/homecook/repository/CustomShopRepositoryImpl.java
+++ src/main/java/com/cotato/homecook/repository/CustomShopRepositoryImpl.java
@@ -15,38 +15,38 @@ import java.util.List;
 import static com.cotato.homecook.domain.entity.QOrderHistory.orderHistory;
 import static com.cotato.homecook.domain.entity.QShop.shop;
 
-@Repository
-@RequiredArgsConstructor
-public class CustomShopRepositoryImpl implements CustomShopRepository {
-    private final JPAQueryFactory jpaQueryFactory;
-    private final JPASQLQuery jpaSqlQuery;
-//    public CustomShopRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
-//        this.jpaQueryFactory = jpaQueryFactory;
+//@Repository
+//@RequiredArgsConstructor
+//public class CustomShopRepositoryImpl implements CustomShopRepository {
+//    private final JPAQueryFactory jpaQueryFactory;
+//    private final JPASQLQuery jpaSqlQuery;
+////    public CustomShopRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
+////        this.jpaQueryFactory = jpaQueryFactory;
+////
+////    }
 //
+//    @Override
+//    public List<ShopRankResponse> findTop10ShopsByOrderCount(double userLatitude, double userLongitude) {
+//        return jpaSqlQuery
+//                .select(Projections.constructor(ShopRankResponse.class,
+//                        shop.shopId, shop.shopName
+//                        , orderHistory.count().as("orderCount")
+//                        , SQLExpressions.rowNumber().over().orderBy(orderHistory.count().desc()).as("ranking")
+//                ))
+//                .from(shop)
+//                .join(orderHistory).on(shop.shopId.eq(orderHistory.shop.shopId))
+//                .where(Expressions.stringTemplate("ST_Distance_Sphere({0}, {1})",
+//                        Expressions.stringTemplate("POINT({0}, {1})",
+//                                userLongitude,
+//                                userLatitude
+//                        ),
+//                        Expressions.stringTemplate("POINT({0}, {1})",
+//                                shop.longitude,
+//                                shop.latitude
+//                        )
+//                ).loe(String.valueOf(3000)))
+//                .groupBy(shop.shopId)
+//                .orderBy(orderHistory.count().desc())
+//                .fetch();
 //    }
-
-    @Override
-    public List<ShopRankResponse> findTop10ShopsByOrderCount(double userLatitude, double userLongitude) {
-        return jpaSqlQuery
-                .select(Projections.constructor(ShopRankResponse.class,
-                        shop.shopId, shop.shopName
-                        , orderHistory.count().as("orderCount")
-                        , SQLExpressions.rowNumber().over().orderBy(orderHistory.count().desc()).as("ranking")
-                ))
-                .from(shop)
-                .join(orderHistory).on(shop.shopId.eq(orderHistory.shop.shopId))
-                .where(Expressions.stringTemplate("ST_Distance_Sphere({0}, {1})",
-                        Expressions.stringTemplate("POINT({0}, {1})",
-                                userLongitude,
-                                userLatitude
-                        ),
-                        Expressions.stringTemplate("POINT({0}, {1})",
-                                shop.longitude,
-                                shop.latitude
-                        )
-                ).loe(String.valueOf(3000)))
-                .groupBy(shop.shopId)
-                .orderBy(orderHistory.count().desc())
-                .fetch();
-    }
-}
+//}
