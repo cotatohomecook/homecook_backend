7140b5be60df66ad9d09b23e786616b503cac506
lildev1-admin <skap0329@naver.com>
2023-05-24 18:17:44 +0900

    :fire: BookmarkResponseInterface 삭제

--- src/main/java/com/cotato/homecook/repository/BookmarkRepository.java
+++ src/main/java/com/cotato/homecook/repository/BookmarkRepository.java
@@ -1,6 +1,5 @@
 package com.cotato.homecook.repository;
 
-import com.cotato.homecook.domain.dto.bookmark.BookmarkResponseInterface;
 import com.cotato.homecook.domain.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark,Long> {
-    List<BookmarkResponseInterface> findByCustomer_CustomerId(Long customerId);
+    List<Bookmark> findByCustomer_CustomerId(Long customerId);
 }
