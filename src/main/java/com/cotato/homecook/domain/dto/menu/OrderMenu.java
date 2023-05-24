0bf30c855560e0b277bf6172c62964462bf2e06e
lildev1-admin <skap0329@naver.com>
2023-05-24 19:05:45 +0900

    :recycle: 주문 메서드 구현 시 toEntity 사용하도록 변경

--- src/main/java/com/cotato/homecook/domain/dto/menu/OrderMenu.java
+++ src/main/java/com/cotato/homecook/domain/dto/menu/OrderMenu.java
@@ -1,5 +1,8 @@
 package com.cotato.homecook.domain.dto.menu;
 
+import com.cotato.homecook.domain.entity.Menu;
+import com.cotato.homecook.domain.entity.OrderHistory;
+import com.cotato.homecook.domain.entity.OrderQuantity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,4 +11,11 @@ import lombok.Data;
 public class OrderMenu {
     Long menuId;
     Long quantity;
+
+    public OrderQuantity toEntity(OrderHistory orderHistory, Menu menu){
+        return OrderQuantity.builder()
+                .orderHistory(orderHistory)
+                .menu(menu)
+                .quantity(quantity).build();
+    }
 }
--- src/main/java/com/cotato/homecook/service/ValidateService.java
+++ src/main/java/com/cotato/homecook/service/ValidateService.java
@@ -1,9 +1,11 @@
 package com.cotato.homecook.service;
 
+import com.cotato.homecook.domain.entity.Menu;
 import com.cotato.homecook.domain.entity.OrderHistory;
 import com.cotato.homecook.domain.entity.Shop;
 import com.cotato.homecook.exception.AppException;
 import com.cotato.homecook.exception.ErrorCode;
+import com.cotato.homecook.repository.MenuRepository;
 import com.cotato.homecook.repository.OrderHistoryRepository;
 import com.cotato.homecook.repository.ShopRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class ValidateService {
     private final OrderHistoryRepository orderHistoryRepository;
     private final ShopRepository shopRepository;
+    private final MenuRepository menuRepository;
 
     public OrderHistory findOrderHistoryById(Long orderHistoryId) {
         return orderHistoryRepository.findById(orderHistoryId)
@@ -29,5 +32,9 @@ public class ValidateService {
     public Shop validateShop(Long shopId){
         return shopRepository.findById(shopId)
                 .orElseThrow(()-> new AppException(ErrorCode.SHOP_NOT_FOUND));
+    }
+
+    public Menu validateMenu(Long menuId){
+        return menuRepository.findById(menuId).orElseThrow(()-> new AppException(ErrorCode.MENU_NOT_FOUND));
     }
 }
