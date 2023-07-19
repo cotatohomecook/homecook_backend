c39f4f3391f4c27b141682fc6b21afad19cc4360
qjvk2880 <qjvk2880@gmail.com>
2023-07-20 02:29:38 +0900

    Feat : 권한을 위한 ENUM 추가

--- src/main/java/com/cotato/homecook/domain/entity/Customer.java
+++ src/main/java/com/cotato/homecook/domain/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.cotato.homecook.domain.entity;
 
+import com.cotato.homecook.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -35,4 +36,7 @@ public class Customer {
     private String email;
     private String password;
     private String phoneNumber;
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
